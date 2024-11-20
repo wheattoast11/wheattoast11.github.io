982921b407b7e72344b9b1d2598c39a170ce0fdc
wheattoast11 <tdesai1994@gmail.com>
2024-11-20 11:53:26 -0600

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/wheattoast11.github.io/', // Replace with your actual repository name
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
