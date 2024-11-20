a94b7fa830917f64c6b78ad46468cda4cbf11d96
wheattoast11 <tdesai1994@gmail.com>
2024-11-20 11:50:54 -0600

    vite distribution

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/wheattoast11.github.io/', // Replace with your actual repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+})
