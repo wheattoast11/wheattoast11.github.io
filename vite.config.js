f23e20ffe6d8bd59c6fc32da34cf7cbe5014a89e
wheattoast11 <tdesai1994@gmail.com>
2024-11-20 15:23:58 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/wheattoast11.github.io/',
+  base: '/',  // Changed from '/wheattoast11.github.io/'
   plugins: [react()],
   build: {
     outDir: 'dist'
