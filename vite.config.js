8884737d43061ddd69387eeed79215101d766485
wheattoast11 <tdesai1994@gmail.com>
2024-11-20 17:58:28 -0600

    custom url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/wheattoast11.github.io/', // Updated to match your repository name
+  base: '/', // Update to root path since custom domain is working
   plugins: [react()],
   build: {
     outDir: 'dist'
