b50d7fb405a2cbd0341c3841e07e7b48c2c979f8
wheattoast11 <tdesai1994@gmail.com>
2024-11-20 18:33:31 -0600

    update createRoot

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Manifesto from './pages/Manifesto';
