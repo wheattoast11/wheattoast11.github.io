0f75d636ed646fe41a1607756246cb4f38f849fa
wheattoast11 <tdesai1994@gmail.com>
2024-11-30 13:22:27 -0600

    updated manifest

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './style.css';
+import '../style.css';
 
 // Create React root and render App
 const root = createRoot(document.getElementById('root'));
